f86de8ba20a8fb66e536e8540b3e03c68bc7790b
Raul Sebastian Mihaila <raul.mihaila@endava.com>
2020-03-11 12:31:58 +0200

    Javascript fundamentals - session 2 + updates to session 1

--- session_ii/javascript-fundamentals/session1/code/modules.js
+++ session_ii/javascript-fundamentals/session1/code/modules.js
@@ -2,6 +2,13 @@
 import { clothes } from "./goods.js";*/
 
 import importedFruits, { clothes, cars, planes, f, A } from "./goods.js";
+// import { clothes as something } from "./goods.js";
+import * as moduleObject from "./goods.js";
+
+console.log("imported module object", moduleObject);
+
+// clothes = 2;
+// moduleObject.clothes = 2;
 
 console.log(importedFruits);
 console.log(clothes);
